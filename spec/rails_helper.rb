59c2732c8be6441491f206052b4c0f565ec7d23d
nexspro <nexspro@ik.me>
2026-07-10 00:32:28 +0200

    test: add SimpleCov coverage (#63)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,15 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start 'rails' do
+  enable_coverage :branch
+
+  add_filter '/bin/'
+  add_filter '/config/'
+  add_filter '/db/'
+  add_filter '/spec/'
+
+  minimum_coverage 70
+end
+
 require 'spec_helper'
 abort('The Rails environment is running in production mode!') if ENV['RAILS_ENV'] == 'production'
 
@@ -15,14 +26,9 @@ elsif ENV['DATABASE_URL']
 end
 
 require_relative '../config/environment'
-# Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-# Uncomment the line below in case you have `--require rails_helper` in the `.rspec` file
-# that will avoid rails generators crashing because migrations haven't been run yet
-# return unless Rails.env.test?
 require 'rspec/rails'
 require 'shoulda/matchers'
-# Add additional requires below this line. Rails is not loaded until this point!
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
